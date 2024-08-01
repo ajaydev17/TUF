a64463dc7d5432e0203005e103d45aeb76a71649
Ajay Dev <devadigaajay1729@gmail.com>
2024-08-02 00:08:09 +0530

    Added pascal star triangle patterns

--- Pattern/star_pascal_triangle_inverse.py
+++ Pattern/star_pascal_triangle_inverse.py
@@ -1,8 +1,8 @@
-#     *
-#    ***
-#   *****
-#  *******
 # *********
+#  *******
+#   *****
+#    ***
+#     *
 
 # prints the above pattern
 
@@ -10,15 +10,15 @@ def print_triangle(n: int) -> None:
     for i in range(0, n):
 
         # print space
-        for _ in range(0, n - i - 1):
+        for _ in range(0, i):
             print(' ', end='')
 
         # print *
-        for _ in range(0, 2 * i + 1):
+        for _ in range(0, 2 * n - 2 * i - 1):
             print('*', end='')
 
         # print space
-        for _ in range(0, n - i - 1):
+        for _ in range(0, i):
             print(' ', end='')
 
         print()
