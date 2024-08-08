fb3509b82bf9944022db4d93b0d8604fd9d4af1f
Ajay Dev <devadigaajay1729@gmail.com>
2024-08-09 00:00:22 +0530

    Added all the pattern problems

---
+++ Pattern/alphabet_triangle_5.py
@@ -0,0 +1,22 @@
+# E
+# D E
+# C D E
+# B C D E
+# A B C D E
+
+
+
+# prints the above pattern
+
+def print_triangle(n: int) -> None:
+
+    for i in range(0, n):
+
+        for j in range(0, i + 1):
+            alphabet = chr(65 + n - i + j - 1)
+            print(alphabet, end=' ')
+
+        print()
+
+
+print_triangle(5)
