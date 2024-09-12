eaa95acd08a7fcaaa21c4b1c9dc5298cf2240037
Ajay Dev <devadigaajay1729@gmail.com>
2024-09-12 23:16:23 +0530

    Added bubble sort

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+
+    for i in range(len(arr)):
+        swapped = False
+
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+
+my_array = [13, 46, 24, 52, 20, 9]
+bubble_sort(my_array)
+print(my_array)
