66ccf55f35d97340778ea08ea963e40ce7ddf031
Ajay Dev <devadigaajay1729@gmail.com>
2024-09-18 00:11:53 +0530

    Added quick sort

---
+++ Sorting/quick_sort_descending.py
@@ -0,0 +1,35 @@
+def get_pivot_index(arr, low, high):
+    pivot = arr[low]
+    i = low
+    j = high
+
+    while i < j:
+
+        while arr[i] >= pivot and i <= high - 1:
+            i = i+1
+
+        while arr[j] < pivot and j >= low + 1:
+            j = j - 1
+
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[low], arr[j] = arr[j], arr[low]
+    return j
+
+
+
+def quick_sort(arr, low, high):
+
+    if low < high:
+
+        pivot_index = get_pivot_index(arr, low, high)
+
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+
+
+
+my_array = [13, 46, 24, 52, 20, 9]
+quick_sort(my_array, 0, len(my_array) - 1)
+print(my_array)
