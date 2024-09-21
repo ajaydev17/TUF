eb2ee49d6dac3ec64df75cbe000bc47094205763
Ajay Dev <devadigaajay1729@gmail.com>
2024-09-21 19:02:11 +0530

    Added linked list insert operations

---
+++ Linked List/linked_list_operations.py
@@ -0,0 +1,85 @@
+# node definition
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# linked list definition
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        result = ''
+        temp_node = self.head
+
+        while temp_node is not None:
+            result = result + str(temp_node.value)
+
+            if temp_node.next is not None:
+                result = result + ' --> '
+            temp_node = temp_node.next
+
+        return result
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length = self.length + 1
+
+        return self.length
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length = self.length + 1
+
+        return self.length
+
+    def insert(self, value, index):
+        if index < 1:
+            return "Invalid index provided."
+
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 1:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+
+            for _ in range(index - 1):
+                temp_node = temp_node.next
+
+            temp_node_next = temp_node.next
+            new_node.next = temp_node_next
+            temp_node.next = new_node
+
+        self.length = self.length + 1
+
+        return self.length
+
