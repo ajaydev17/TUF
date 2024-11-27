8a4f0784b4861229baf501f3fc7f64733f44ecb9
clutch_god17 <devadigaajay1729@gmail.com>
2024-11-27 23:52:35 +0530

    blind 75 maximum subarray product.

--- Blind 75/maximun_subarray_product.py
+++ Blind 75/maximun_subarray_product.py
@@ -16,3 +16,43 @@ def max_subarray_brute_force(nums):
             max_product = max(max_product, current_product)
 
     return max_product
+
+# maximum subarray product using better solution
+# use two loops
+# time complexity is O(n*2) and space complexity is O(1)
+
+def max_subarray_better_solution(nums):
+    max_product = float('-inf')
+
+    for i in range(len(nums)):
+        current_product = 1
+        for j in range(i, len(nums)):
+            current_product = current_product * nums[j]
+            max_product = max(max_product, current_product)
+
+    return max_product
+
+# maximum subarray product using optimal solution
+# using prefix product and suffix product
+# time complexity is O(n) and space complexity is O(1)
+
+def max_subarray_optimal_solution(nums):
+    max_product = float('-inf')
+    prefix_product = 1
+    suffix_product = 1
+
+    for i in range(len(nums)):
+        if prefix_product == 0:
+            prefix_product = 1
+
+        if suffix_product == 0:
+            suffix_product = 1
+
+        prefix_product = prefix_product * nums[i]
+        suffix_product = suffix_product * nums[len(nums) - i - 1]
+        max_product = max(max_product, max(prefix_product, suffix_product))
+
+    return max_product
+
+
+
