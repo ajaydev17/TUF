b66b1de5e6dc0b052bcfaf56463a7c05e3483921
clutch_god17 <devadigaajay1729@gmail.com>
2024-12-10 23:51:18 +0530

    blind 75 three sum equals to zero.

--- Blind 75/three_sum.py
+++ Blind 75/three_sum.py
@@ -40,3 +40,37 @@ def three_sum_better(nums):
         result = [list(item) for item in hash_set]
 
         return result
+
+# three sum using optimal solution
+# sort the array start an index i=0, j=i+1 and k=n-1
+# time complexity is O(n*logn) and space complexity is O(1)
+
+def three_sum_optimal(nums):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            if nums[i] + nums[left] + nums[right] == 0:
+                result.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+            elif nums[i] + nums[left] + nums[right] < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return result
