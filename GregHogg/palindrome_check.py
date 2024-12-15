654dbdf69b02d18dd1b7e82a8ec8e2a3c6ca1589
clutch_god17 <devadigaajay1729@gmail.com>
2024-12-15 14:49:52 +0530

    palindrome check better solution.

--- GregHogg/palindrome_check.py
+++ GregHogg/palindrome_check.py
@@ -10,3 +10,24 @@ def palindrome_check_brute_force(my_str):
         return True
     else:
         return False
+
+
+# better solution using two pointers
+# time complexity O(n) and space complexity O(1)
+
+def palindrome_check_better(my_str):
+    # initialize pointers at the beginning and end of the string respectively
+    left = 0
+    right = len(my_str) - 1
+
+    # compare characters from both ends of the string until they meet in the middle or cross each other
+    while left < right:
+        # if characters are not equal, the string is not a palindrome
+        if my_str[left] != my_str[right]:
+            return False
+
+        # move the pointers towards each other
+        left += 1
+        right -= 1
+
+    return True
