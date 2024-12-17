a6da801f5d17dc5b711060e17d067bf7a4f62fc2
clutch_god17 <devadigaajay1729@gmail.com>
2024-12-17 23:49:42 +0530

    square of sorted array optimal solution.

--- GregHogg/square_of_sorted_array.py
+++ GregHogg/square_of_sorted_array.py
@@ -1,2 +1,16 @@
 # square of a sorted array using two pointers approach
 # time complexity O(n) and space complexity O(n)
+
+def square_sorted_array(nums):
+    result = [0] * len(nums)
+    left, right = 0, len(nums) - 1
+
+    for i in range(len(nums) - 1, -1, -1):
+        if abs(nums[left]) > abs(nums[right]):
+            result[i] = nums[left] * nums[left]
+            left += 1
+        else:
+            result[i] = nums[right] * nums[right]
+            right -= 1
+
+    return result
