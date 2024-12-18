c742b46dd3c6fa3aa2a7d2efc180cd15a5636ff8
clutch_god17 <devadigaajay1729@gmail.com>
2024-12-18 23:45:45 +0530

    square of sorted array optimal solution.

--- GregHogg/square_of_sorted_array.py
+++ GregHogg/square_of_sorted_array.py
@@ -2,10 +2,19 @@
 # time complexity O(n) and space complexity O(n)
 
 def square_sorted_array(nums):
+    # initialize an empty list with zero values
     result = [0] * len(nums)
+
+    # initialize pointers at the beginning and end of the string respectively
     left, right = 0, len(nums) - 1
 
+    # iterate over the array in reverse order and calculate the square of each element
     for i in range(len(nums) - 1, -1, -1):
+
+        # calculate the square of the element pointed by the left pointer if it's larger,
+        # otherwise, calculate the square of the element pointed by the right pointer
+        # and increment the pointers accordingly.
+        # and add the value at corresponding index in result list
         if abs(nums[left]) > abs(nums[right]):
             result[i] = nums[left] * nums[left]
             left += 1
