c2da8347be59c246a136312c570179542b8d8c5d
clutch_god17 <devadigaajay1729@gmail.com>
2024-12-27 01:40:34 +0530

    reverse string with constant space.

--- GregHogg/reverse_string.py
+++ GregHogg/reverse_string.py
@@ -2,9 +2,11 @@
 # time complexity O(n) and space complexity O(1)
 
 def reverse_string(my_str):
+    # initialize the left and right pointers
     left = 0
     right = len(my_str) - 1
 
+    # swap characters from the left and right pointers until they meet in the middle
     while left < right:
         my_str[left], my_str[right] = my_str[right], my_str[left]
         left += 1
