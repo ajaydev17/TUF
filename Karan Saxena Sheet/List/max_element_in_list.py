aa6d0f854e4f9f785a14b0ece8adfd1a48dcdb72
clutch_god17 <devadigaajay1729@gmail.com>
2025-02-19 23:11:46 +0530

    max element in a list

---
+++ Karan Saxena Sheet/List/max_element_in_list.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def get_max_element(numbers: List) -> int:
+
+    # initialize a variable with a min value
+    max_element = float('-inf')
+
+    # iterate through the list to find the max element
+    for index, value in enumerate(numbers):
+        max_element = max(max_element, value)
+
+    # return the max element
+    return max_element
+
+# time complexity: O(n)
+# space complexity: O(1)
