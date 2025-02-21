0a10fabe08f2381e01f0d284de96fc182be8cd5c
clutch_god17 <devadigaajay1729@gmail.com>
2025-02-21 14:41:19 +0530

    move zeros to end

--- Karan Saxena Sheet/List/move_zeros_to_end.py
+++ Karan Saxena Sheet/List/move_zeros_to_end.py
@@ -13,3 +13,11 @@ def move_zeros_to_end(numbers: List[int]) -> List[int]:
 
 # time complexity: O(n)
 # space complexity: O(1)
+
+def move_zeros_to_end_list_comprehension(numbers: List[int]) -> List[int]:
+    result = [num for num in numbers if num != 0]
+    result = result + [0] * (len(numbers) - len(result))
+    return result
+
+# time complexity: O(n)
+# space complexity: O(n)
